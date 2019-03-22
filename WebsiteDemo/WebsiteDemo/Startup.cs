c444afad1d23b211e62e03074d9cf1717fcc3a05
nhat hao <nhathao.dong@gmail.com>
2019-03-22 13:17:16 +0700

    Create project WebsiteDemo MVC

---
+++ WebsiteDemo/WebsiteDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteDemo.Startup))]
+namespace WebsiteDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
